61d406f5474c5e9cbe4d9764f07811911cb9a641
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2023-12-27 18:59:05 +0100

    add CRUD operation

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -12,6 +13,35 @@ def post_detail(request,id):
     post = Post.objects.get(id=id)
     return render (request,"single.html",{"data":post})
 
+
+def create_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST,request.FILES)
+        if form.is_valid():
+            myform = form.save(commit=False)
+            myform.author = request.user
+            myform.save()
+    else:
+        form = PostForm()
+    return render(request, "create.html", {"form":form})
+
+
+def edit_post(request,id):
+    post = Post.objects.get(id=id)
+    if request.method == "POST":
+        form = PostForm(request.POST,request.FILES,instance=post)
+        if form.is_valid():
+            myform = form.save(commit=False)
+            myform.author = request.user
+            myform.save()
+    else:
+        form = PostForm(instance=post)
+    return render(request, "edit.html", {"form":form})
+
+def delete_post (request,id):
+    post = Post.objects.get(id=id)
+    post.delete()
+    return redirect ("/blog")
 
 
 
