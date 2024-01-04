8cce762db307cf2af4c52d48ef1ead2e6492f951
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2024-01-04 20:22:14 +0100

    work on frontend

--- about/views.py
+++ about/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from . models import About, Skills
+from . models import About, Skills, Education, Experience 
 # Create your views here.
 
 def home(request):
@@ -11,12 +11,16 @@ def home(request):
     about = About.objects.last()
     coding_skills = Skills.objects.filter(type='Coding')
     design_skills = Skills.objects.filter(type='Design')
+    education = Education.objects.all()
+    experience = Experience.objects.all()
 
 
     return render (request, 'home.html',{
     "about":about ,  
     'coding_skills':coding_skills ,
-    'design_skills':design_skills
+    'design_skills':design_skills ,
+    'education':education,
+    'experience':experience,
 
 
 
